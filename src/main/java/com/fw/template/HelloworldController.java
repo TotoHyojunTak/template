2288912a4f1018f2ba09bb1e15d5bb59b340ade1
hyojuntak <thj0309@hotmail.com>
2022-05-30 17:34:10 +0900

    getmapping 수정

--- src/main/java/com/fw/template/HelloworldController.java
+++ src/main/java/com/fw/template/HelloworldController.java
@@ -1,12 +1,13 @@
 package com.fw.template;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/hello")
 @RestController
 public class HelloworldController {
-    @GetMapping
+    @GetMapping("/")
     public String helloworld(){
         return "hello world";
     }
