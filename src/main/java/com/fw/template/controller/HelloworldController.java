f58572b13e7a5e62012c3fa1aad362eb18d80dbd
thj0309 <thj0309@cj.net>
2022-07-01 14:49:40 +0900

    modified

--- src/main/java/com/fw/template/controller/HelloworldController.java
+++ src/main/java/com/fw/template/controller/HelloworldController.java
@@ -1,4 +1,4 @@
-package com.fw.template;
+package com.fw.template.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloworldController {
     @GetMapping("/")
-    public String helloworld(){
+    public String helloworld() {
         return "hello world";
     }
 }
